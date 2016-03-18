8f10b41a1d28b1de6c99482bb7d57d37c748db97
shashi yebbare <shashi.kshatriyan@gmail.com>
2016-03-18 07:39:55 +0530

    AEM Training

---
+++ bundle/src/test/java/com/cylync/training/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.cylync.training;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
